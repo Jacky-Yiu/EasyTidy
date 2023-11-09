6aeae2e696875f179dde7ec48d06ae1e2195d437
JackyYiu <jackyyiu0810@gmail.com>
2023-11-09 14:43:54 -0800

    Add roxegen

--- R/tidy_column_with_delimiter_seperated_values.R
+++ R/tidy_column_with_delimiter_seperated_values.R
@@ -1,3 +1,15 @@
+#' Tidy Column with Delimiter-Separated Values
+#'
+#' This function separates a column with delimiter-separated values into multiple rows and pivots it to wide format, indicating the presence of each value.
+#'
+#' @param data A tibble/data frame containing the column to be tidied.
+#' @param delimiter The delimiter used to separate values within the column.
+#' @param column The name of the column that contains delimiter-separated values.
+#' @return A modified tibble/data frame where the specified column has been separated into multiple rows and pivoted to a wider format, showing the presence of each 'value.'
+#' @examples
+#' data <- data.frame(ID = 1:3, Codes = c("A|B|C", "D|E", "F|G|H|I"))
+#' tidy_column_with_delimiter_separated_values(data, delimiter = "|", column = Codes)
+#' @export
 tidy_column_with_delimiter_seperated_values <- function(data, delimiter, column) {
   col_name <- deparse(substitute(column))
   stopifnot(col_name %in% colnames(data))
